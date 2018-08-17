285eb7cade2c9dea74ed759c99a60427d6adfa8b
Victor Silva Segarra <vicsilseg@gmail.com>
2018-08-17 18:12:04 +0200

    logos

---
+++ data/logos.js
@@ -0,0 +1,82 @@
+const logos = [
+  {
+    name: "Logo_IVI.png",
+    alt: "Logo IVI"
+  },
+  {
+    name: "Logo_MCO.png",
+    alt: "Logo MCO"
+  },
+  {
+    name: "Logo_Medtronic.png",
+    alt: "Logo Medtronic"
+  },
+  {
+    name: "Logo_Microsoft.png",
+    alt: "Logo Microsoft"
+  },
+  {
+    name: "Logo_Natural-bridges.png",
+    alt: "Logo Natural Bridges"
+  },
+  {
+    name: "Logo_Sabien.png",
+    alt: "Logo Sabien"
+  },
+  {
+    name: "Logo_Samsung.png",
+    alt: "Logo Samsung"
+  },
+  {
+    name: "Logo_Sitium.png",
+    alt: "Logo Sitium"
+  },
+  {
+    name: "Logo_Telefonica.png",
+    alt: "Logo Telefonica"
+  },
+  {
+    name: "Logo_UPV.png",
+    alt: "Logo UPV"
+  },
+  {
+    name: "Logo_Ifedes.png",
+    alt: "Logo Ifedes"
+  },
+  {
+    name: "Logo_Huawei.png",
+    alt: "Logo Huawei"
+  },
+  {
+    name: "Logo_Texas-instruments.png",
+    alt: "Logo Texas Instruments"
+  },
+  {
+    name: "Logo_General_Electric.png",
+    alt: "Logo General Electric"
+  },
+  {
+    name: "Logo_GEE.png",
+    alt: "Logo GEE"
+  },
+  {
+    name: "Logo_Everis.png",
+    alt: "Logo Everis"
+  },
+  {
+    name: "Logo_Dedalus.png",
+    alt: "Logo Dedalus"
+  },
+  {
+    name: "Logo_Clece.png",
+    alt: "Logo Clece"
+  },
+  {
+    name: "Logo_AIPAD.png",
+    alt: "Logo AIPAD"
+  },
+  {
+    name: "Logo_ACEBG.png",
+    alt: "Logo ACEBG"
+  }
+];
