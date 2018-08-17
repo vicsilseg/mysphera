2771745f26648e3e2e8fd7bd7f60a0616fb1c76f
Victor Silva Segarra <vicsilseg@gmail.com>
2018-08-17 18:12:12 +0200

    hpm start

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
 $(document).ready(function() {
   /**/
-
   /* OBJECT DEFINITION */
   var spheres =
     '<div id="spheres"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -6 27 18"><defs><style>.first-sphere{fill:#2293bc;}.second-sphere{fill:#2293bc;}.third-sphere{fill:#25adbc;}</style></defs><title>spheras</title><g id="spheres-container" data-name="spheres container"><circle class="first-sphere" cx="3" cy="3" r="3"/><circle class="second-sphere" cx="13.5" cy="3" r="3"/><circle class="third-sphere" cx="24" cy="3" r="3"/></g></svg></div>';
+  var logotype = function(item, alt){
+    return `<div class="logo-item grid-content inactive"><div class="grey-overlay"></div><img src="./assets/logos/100x300/${item}" alt="${alt}"></div>`;
+  }
+  
 
   /* $('<img/>').attr('src', '../assets/img/Home_background.jpg').on('load', function() {
     $('body').addClass('overlay-active');
@@ -52,7 +56,7 @@ $(document).ready(function() {
    * Function that turns on button by mouse enter
    */
   var enterButton = function() {
-    $(".iot-button").mouseenter(function() {
+    $(".ms-button").mouseenter(function() {
       var $fill = $(this)
         .children("#button-svg")
         .children(".path");
@@ -67,7 +71,7 @@ $(document).ready(function() {
    * Function that turns on button by mouse leave
    */
   var leaveButton = function() {
-    $(".iot-button").mouseleave(function() {
+    $(".ms-button").mouseleave(function() {
       var $fill = $(this)
         .children("#button-svg")
         .children(".path");
@@ -142,6 +146,51 @@ $(document).ready(function() {
     $mobile.css({'background': `linear-gradient(${c1},${c2}), url(${mobile})`,'background-size':'cover'})
     $tablet.css({'background': `linear-gradient(${c3},${c4}), url(${tablet})`,'background-size':'cover'})
   };
+
+  /**
+   * Function that adds logos to section (currently 21)
+   * 
+   */
+  var addLogoToSection = function(logos){
+    var $logos = $('#logo-container');
+    for(var logo of logos){
+      $logos.append(logotype(logo.name,logo.alt))
+    }
+  }
+  /**
+   * On logo mouse enter
+   * 
+   */
+  var eachLogoEnter = function(){
+    $(".logo-item").mouseenter(function() {
+      var $this = $(this);
+      var $logo = $this.children('.grey-overlay');
+      var $img = $this.children('img');
+      $logo.removeClass("grey-overlay");
+      $logo.addClass("active");
+      $this.removeClass("inactive");
+      $img.addClass("img-augmented");
+    });
+  }
+  /**
+   * On logo mouse leave
+   * 
+   */
+  var eachLogoLeave = function(){
+    $(".logo-item").mouseleave(function() {
+      var $this = $(this);
+      var $logo = $this.children('.active');
+      var $img = $this.children('img');
+      $logo.addClass("grey-overlay");
+      $logo.removeClass("active");
+      $this.addClass("inactive");
+      $img.removeClass("img-augmented");
+    });
+  }
+  
+  addLogoToSection(logos);
+  eachLogoEnter();
+  eachLogoLeave();
   overlayOn();
   overlayOff();
   enterButton();
